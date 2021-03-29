7dfe25026b2632154acc2f81c5c86a5ab87ccd4b
Aliaksandr <klut2018a@gmail.com>
2021-03-30 00:25:10 +0300

    Initial commit

---
+++ Cinema Room Manager/task/src/cinema/Cinema.java
@@ -0,0 +1,15 @@
+package cinema;
+
+import java.util.Scanner;
+
+public class Cinema {
+
+    public static void main(String[] args) {
+        // Write your code here
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("  1 2 3 4 5 6 7 8");
+        for (int i = 1; i < 9; i++) {
+            System.out.println(i + " S S S S S S S S");
+        }
+    }
+}
