a9aada74a8d0ddf8c21e6738f5360544237e40e4
Aliaksandr <klut2018a@gmail.com>
2021-03-30 00:29:02 +0300

    Commit. Fixed first out line. add Cinema

--- Cinema Room Manager/task/src/cinema/Cinema.java
+++ Cinema Room Manager/task/src/cinema/Cinema.java
@@ -7,8 +7,9 @@ public class Cinema {
     public static void main(String[] args) {
         // Write your code here
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Cinema:");
         System.out.println("  1 2 3 4 5 6 7 8");
-        for (int i = 1; i < 9; i++) {
+        for (int i = 1; i < 8; i++) {
             System.out.println(i + " S S S S S S S S");
         }
     }
