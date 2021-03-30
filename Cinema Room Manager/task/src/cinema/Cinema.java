c6c53506e5cf4b2f02e69ee52db3020f655dedf4
Aliaksandr <klut2018a@gmail.com>
2021-03-30 17:06:33 +0300

    Commit. Stage 2/5: Sold_

--- Cinema Room Manager/task/src/cinema/Cinema.java
+++ Cinema Room Manager/task/src/cinema/Cinema.java
@@ -7,10 +7,46 @@ public class Cinema {
     public static void main(String[] args) {
         // Write your code here
         Scanner scanner = new Scanner(System.in);
+
+        int rows;
+        int seats;
+
+        System.out.println("Enter the number of rows:");
+        rows = input(scanner);
+        System.out.println("Enter the number of seats in each row:");
+        seats = input(scanner);
+        System.out.println("Total income:");
+
+        System.out.println("$" + income(rows, seats));
+        //printCinema(7, 8);
+    }
+
+    public static int input(Scanner scanner) {
+        return scanner.nextInt();
+    }
+
+    public static void printCinema(int n, int m) {
         System.out.println("Cinema:");
-        System.out.println("  1 2 3 4 5 6 7 8");
-        for (int i = 1; i < 8; i++) {
-            System.out.println(i + " S S S S S S S S");
+        System.out.print(" ");
+        for (int i = 0; i < m; i++) {
+            System.out.print(" " + (i + 1));
         }
+        System.out.println();
+        for (int i = 1; i <= n; i++) {
+            System.out.print(i);
+            for (int j = 0; j < m; j++) {
+                System.out.print(" S");
+            }
+            System.out.println();
+        }
+    }
+
+    public static int income(int n, int m) {
+        if (n * m > 60) {
+            return (n / 2) * m * 10 + (n - n / 2) * m * 8;
+        } else {
+            return n * m * 10;
+        }
+
     }
 }
