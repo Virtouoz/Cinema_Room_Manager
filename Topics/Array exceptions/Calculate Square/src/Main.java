2699f521e7444f618aa2a6c25732f5ff13862269
Aliaksandr <klut2018a@gmail.com>
2021-03-31 19:21:40 +0300

    add task: Stage 5/5: Errors_ finish task: Calculate Square

--- Topics/Array exceptions/Calculate Square/src/Main.java
+++ Topics/Array exceptions/Calculate Square/src/Main.java
@@ -1,7 +1,19 @@
 class FixingExceptions {
 
+//    public static void main(String[] args) {
+//        int[] array = new int[5];
+//        int index = 1;
+//        calculateSquare(array, index);
+//    }
+
     public static void calculateSquare(int[] array, int index) {
         // write your code here
-        if()
+        if (array == null) {
+            System.out.println("Exception!");
+        } else if (array.length - 1 < index || index < 0) {
+            System.out.println("Exception!");
+        } else {
+            System.out.println(array[index] * array[index]);
+        }
     }
 }
